3a2de9fbe492d06bbe9952174d6ce9042260bf71
unknown <alejandrojzeballos@gmail.com>
2021-03-04 23:50:12 -0400

    {add} Descripcion de la herramietna y proyectos

--- apuntes/apuntes.php
+++ apuntes/apuntes.php
@@ -1,3 +1,19 @@
 <?php
 echo "Hello laravel";
-/* Implementaciond e api con tdd */
+/* Implementaciond e api con tdd 
+    Laravel esta construido en php.
+    Laravel es una herramietna que permite desarrollar un sistema web.
+    Laravel tiene por defecto inicio de sessiones por fb. es un framework con muchas ventajas.
+    capas
+    ruta        ruta            ruta
+    vista       controller      midldleware
+                view            controller
+                                View
+
+                                Proyectos
+    - Crud con laravel
+    - Registro de post
+    - API
+
+
+*/
