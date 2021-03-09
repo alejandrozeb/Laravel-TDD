2711ff9631e1aa1170927986adfac2af5f8d6074
unknown <alejandrojzeballos@gmail.com>
2021-03-08 22:16:18 -0400

    {add} añadiendo controlador de index en UserController

--- sites/ciclo/app/Http/Controllers/UserController.php
+++ sites/ciclo/app/Http/Controllers/UserController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(){
-
+        $user = User::latest()->get();
+        //no interactuamos con codido sql
+        return view('users.index', [
+            'users' => $user
+        ]);
     }
     public function store(){
         
