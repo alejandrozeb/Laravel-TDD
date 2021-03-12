863cd575b2cb3138e39836826c986aa01948d748
unknown <alejandrojzeballos@gmail.com>
2021-03-11 22:34:34 -0400

    {add} Validando Request

--- sites/formrequest/app/Http/Controllers/PostController.php
+++ sites/formrequest/app/Http/Controllers/PostController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
-    public function store(Request $request){
+    public function store(PostRequest $request){
         dd($request->all());
     }
 }
