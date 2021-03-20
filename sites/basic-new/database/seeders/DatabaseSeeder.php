948d97f8d6020cdd6106ffc311944040a2065ad5
unknown <alejandrojzeballos@gmail.com>
2021-03-19 22:18:44 -0400

    {add} Agregando el seeder para usuario y post

--- sites/basic-new/database/seeders/DatabaseSeeder.php
+++ sites/basic-new/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        App\User::create([
+            'name' => 'Italo Morales F',
+            'email' => 'i@admin.com',
+            'password'=> bcrypt('123456') 
+        ]);
+        //no vamos a saber el email si usamos el factory de usuarios
+        factory(App\Post::class, 24)->create();
     }
 }
