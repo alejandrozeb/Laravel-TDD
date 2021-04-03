5ccee68a1e31f1e8802026030b8aa50af4a03b77
unknown <alejandrojzeballos@gmail.com>
2021-04-02 23:44:41 -0400

    {add} pasando la prueba test_delete con el controlador destroy

--- sites/api-new/app/Http/Controllers/Api/PostController.php
+++ sites/api-new/app/Http/Controllers/Api/PostController.php
@@ -72,6 +72,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return response()->json(null, 204);
     }
 }
